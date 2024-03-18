3098fa6274b91bdf3297f031b30b3f19f43709d9
renesalomone <108975981+renesalomone@users.noreply.github.com>
2024-03-18 13:15:15 +0700

    Initial Commit

---
+++ RoutePlanningAppSwiftUI/RoutePlanningAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutePlanningAppSwiftUIApp.swift
+//  RoutePlanningAppSwiftUI
+//
+//  Created by Rene Salomone on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct RoutePlanningAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
